6f4d8d165b4e8f70775acfb4087379ed5ff625dd
Omer Karisman <omer.karisman@getir.com>
2016-04-19 13:27:31 +0300

    v1

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var channelId = "";
 var sendMessage = function(message){
 	var url = "https://api.telegram.org/bot201093357:AAE6Zy0V-g7kuuYHp0Owl8LXo3FqKBtXXsE/sendMessage?chat_id=@okbottest&text=" + message;
 	https.get(url, (res) => {
+		console.log('If you want to keep your Telegram Channel private. Provide this as your channel id to this module:' + res.data.result.chat.id);
 	  console.log(`Got response: ${res.statusCode}`);
 	  // consume response body
 	  res.resume();
