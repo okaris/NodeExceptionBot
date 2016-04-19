98c3169e17c245f90bae572c00234b1ff5de1faf
Omer Karisman <omer.karisman@getir.com>
2016-04-19 13:17:28 +0300

    v1

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+const https = require('https');
+
+var channelId = "";
+
+var sendMessage = function(message){
+	var url = "https://api.telegram.org/bot201093357:AAE6Zy0V-g7kuuYHp0Owl8LXo3FqKBtXXsE/sendMessage?chat_id=@okbottest&text=" + message;
+	https.get(url, (res) => {
+	  console.log(`Got response: ${res.statusCode}`);
+	  // consume response body
+	  res.resume();
+	}).on('error', (e) => {
+	  console.log(`Got error: ${e.message}`);
+	});
+}
+
+sendMessage("Bot started successfully");
+
 process.on('uncaughtException', function (err) {
-  console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
-  console.error(err.stack)
-  process.exit(1)
+	sendMessage((new Date).toUTCString() + ' uncaughtException:', err.message);
+	// console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
+	// console.error(err.stack)
+	process.exit(1)
 })
