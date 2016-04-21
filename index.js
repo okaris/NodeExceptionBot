d21e2fd6d3ddcd29ac85d6f5e8d53ddd83a1d711
Omer Karisman <omer.karisman@getir.com>
2016-04-21 15:48:42 +0300

    optimization

--- index.js
+++ index.js
@@ -8,35 +8,28 @@ var init = function(key, name){
 	}
 
 	var sendMessage = function(message, cb){
+		console.log(message);
 		var url = "https://node-exception-bot.herokuapp.com/exception?token=" + apiKey + "&message=" + message;
 		
 		request.get(url, function (error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    var response = JSON.parse(body);
-	    	if(cb){
-	   			cb(response);
-	    	}
-		  }else{
-		  	console.log(response);
-		  }
+			if(error){
+				console.log(error);
+			}
+			cb(response);
 		});
 	}
-
-	sendMessage(prefix + "Node Exception Bot started successfully.");
+	sendMessage(prefix + (new Date).toUTCString() + " Node Exception Bot started successfully.");
 
 	var sendErrorMessage = function(err){
 		sendMessage(err,function(){
-			console.log("exit");
 			process.exit();
 		});
 	}
 
 	process.on('uncaughtException', function (err) {
-		sendErrorMessage(prefix + (new Date).toUTCString() + ' uncaughtException: ' + err.stack);
-		//console.log((new Date).toUTCString() + ' uncaughtException:', err.stack);
-		// console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
-		// console.error(err.stack)
-		//process.exit(1)
+		setTimeout(function(){
+			sendErrorMessage(prefix + (new Date).toUTCString() + ' uncaughtException: ' + err.stack);
+		},100);
 	})
 };
 
